4faffbc0d219ac8c25e23ed50a607b86d6b0a2e0
Marcus Price <marcusprice88@gmail.com>
2019-09-21 23:16:49 -0700

    both contact forms now fully functional

--- client/src/components/ContactFormCard.jsx
+++ client/src/components/ContactFormCard.jsx
@@ -1,9 +1,84 @@
-import React from 'react'
-import { FullCard, FormContainer, FormInfo, CardTitle, FormParagraph, ContactForm, FormLabel, Input, Submit, TextArea } from './Atoms.jsx'
+import React, { useState } from 'react'
+import { FullCard, FormContainer, FormInfo, CardTitle, FormParagraph, Paragraph, ContactForm, FormLabel, Input, Submit, TextArea, Modal, ModalCard, ModalButton } from './Atoms.jsx'
 
 const ContactFormCard = (props) => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [subject, setSubject] = useState('')
+  const [message, setMessage] = useState('')
+  const [showModal, setShowModal] = useState(false)
+  const [success, setSuccess] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    sendEmail({
+      name: name,
+      email: email,
+      subjectType: 'Contact',
+      subject: subject,
+      message: message
+    })
+  }
+
+  const sendEmail = (formValues) => {
+    fetch('/sendEmail', {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        redirect: 'follow',
+        referrer: 'client',
+        body: JSON.stringify(formValues)
+    })
+      .then(response => response.json())
+      .then((result) => {
+        if(result.emailSent) {
+          setName('')
+          setEmail('')
+          setSubject('')
+          setMessage('')
+          setSuccess(true)
+          setShowModal(true)
+        } else {
+          setSuccess(false)
+          setShowModal(true)
+        }
+      })
+  }
+
+  const handleModal = () => {
+    if(showModal) {
+      if(success) {
+        return(
+          <Modal onClick={() => {setShowModal(false)}}>
+            <ModalCard>
+              <CardTitle>Message Sent</CardTitle>
+              <Paragraph>Your message to Dustin was successfully sent. You can expect a response within the next few days.</Paragraph>
+              <ModalButton onClick={() => {setShowModal(false)}}>Close</ModalButton>
+            </ModalCard>
+          </Modal>
+        )
+      } else {
+        return(
+          <Modal onClick={() => {setShowModal(false)}}>
+            <ModalCard>
+              <CardTitle>Error!</CardTitle>
+              <Paragraph>There was an error sending your message. Please double-check the form and try again.</Paragraph>
+              <ModalButton onClick={() => {setShowModal(false)}}>Close</ModalButton>
+            </ModalCard>
+          </Modal>
+        )
+      }
+    }
+  }
+
   return(
     <FullCard>
+      {handleModal()}
       <FormContainer>
 
         <FormInfo>
@@ -13,15 +88,15 @@ const ContactFormCard = (props) => {
           </FormParagraph>
         </FormInfo>
 
-        <ContactForm autocomplete="off">
+        <ContactForm required="required" onSubmit={handleSubmit} autocomplete="off">
           <FormLabel top={true}>Name</FormLabel>
-          <Input top={true} type="text" />
+          <Input top={true} value={name} onChange={(e) => {setName(e.target.value)}}  type="text" />
           <FormLabel>Email</FormLabel>
-          <Input type="email" />
+          <Input type="email" value={email} onChange={(e) => {setEmail(e.target.value)}}  />
           <FormLabel>Subject</FormLabel>
-          <Input type="text" />
+          <Input type="text" value={subject} onChange={(e) => {setSubject(e.target.value)}}  />
           <FormLabel>Message</FormLabel>
-          <TextArea />
+          <TextArea value={message} onChange={(e) => {setMessage(e.target.value)}} />
           <Submit value="Send Message" type="submit"/>
         </ContactForm>
 
